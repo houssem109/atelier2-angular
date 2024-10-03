7abf7171001e336406a2a9092c518f2869b42cd1
houssem_ben_jaafar <houssembenjaafar105@gmail.com>
2024-10-03 20:38:17 +0100

    section 9 done

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+})
+export class ProduitsComponent implements OnInit {
+  produits? : Produit[]
+  private produitService= new ProduitService()
+
+  constructor( ) {
+    //this.produits=[]
+    }
+  ngOnInit(): void {
+    this.produits = this.produitService.listeProduits();
+
+  }
+}
